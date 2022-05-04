0f12e8c3d5d38642ededc42699c4f686e341b777
remorses <beats.by.morse@gmail.com>
2022-05-04 19:04:56 +0200

    fix code style

--- lib/index.ts
+++ lib/index.ts
@@ -83,25 +83,23 @@ export default function init(withRpcConfig: WithRpcConfig = {}) {
   };
 }
 
-
-
 // taken from https://github.com/vercel/next.js/blob/v12.1.5/packages/next/lib/find-pages-dir.ts
 export function findPagesDir(dir: string): string {
   // prioritize ./pages over ./src/pages
-  let curDir = path.join(dir, 'pages')
-  if (fs.existsSync(curDir)) return curDir
+  let curDir = path.join(dir, 'pages');
+  if (fs.existsSync(curDir)) return curDir;
 
-  curDir = path.join(dir, 'src/pages')
-  if (fs.existsSync(curDir)) return curDir
+  curDir = path.join(dir, 'src/pages');
+  if (fs.existsSync(curDir)) return curDir;
 
   // Check one level up the tree to see if the pages directory might be there
   if (fs.existsSync(path.join(dir, '..', 'pages'))) {
-      throw new Error(
-          'No `pages` directory found. Did you mean to run `next` in the parent (`../`) directory?',
-      )
+    throw new Error(
+      'No `pages` directory found. Did you mean to run `next` in the parent (`../`) directory?'
+    );
   }
 
   throw new Error(
-      "Couldn't find a `pages` directory. Please create one under the project root",
-  )
+    "Couldn't find a `pages` directory. Please create one under the project root"
+  );
 }
